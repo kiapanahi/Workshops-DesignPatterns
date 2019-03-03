f7666a4bf858998f38a0b34f0f3a29eb18c35f9c
Kia Panahi Rad <kia.panahirad@gmail.com>
2019-03-03 16:04:39 +0330

    Add sample decorator implementation

---
+++ src/Decorator/Instagramock/Picture.cs
@@ -0,0 +1,84 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace Decorator.Instagramock
+{
+    public interface IPicture
+    {
+        List<string> Filters { get; set; }
+        double SizeOnDisk { get; set; }
+    }
+
+    public class RawPicture : IPicture
+    {
+        public RawPicture(double size)
+        {
+            Filters = new List<string>();
+            SizeOnDisk = size;
+        }
+
+        public List<string> Filters { get; set; }
+        public double SizeOnDisk { get; set; }
+    }
+
+    public class SepiaFilter : IPicture
+    {
+        public SepiaFilter(IPicture picture)
+        {
+            Filters = picture.Filters.Union(new[] { "Sepia" }).ToList();
+            SizeOnDisk = picture.SizeOnDisk + picture.SizeOnDisk * .4;
+        }
+
+        public List<string> Filters { get; set; }
+        public double SizeOnDisk { get; set; }
+    }
+
+    public class GrayScaleFilter : IPicture
+    {
+        public GrayScaleFilter(IPicture picture)
+        {
+            Filters = picture.Filters.Union(new []{"Gray-Scale"}).ToList();
+            SizeOnDisk = picture.SizeOnDisk + picture.SizeOnDisk*.2;
+        }
+
+        public List<string> Filters { get; set; }
+        public double SizeOnDisk { get; set; }
+    }
+
+    public class RedEmphasizeFilter : IPicture
+    {
+        public RedEmphasizeFilter(IPicture picture)
+        {
+            Filters = picture.Filters.Union(new[] { "Red-Emphasized" }).ToList();
+            SizeOnDisk = picture.SizeOnDisk + picture.SizeOnDisk * .4;
+        }
+
+        public List<string> Filters { get; set; }
+        public double SizeOnDisk { get; set; }
+    }
+
+    public class BlueEmphasizeFilter : IPicture
+    {
+        public BlueEmphasizeFilter(IPicture picture)
+        {
+            Filters = picture.Filters.Union(new[] { "Blue-Emphasized" }).ToList();
+            SizeOnDisk = picture.SizeOnDisk + picture.SizeOnDisk * .45;
+        }
+
+        public List<string> Filters { get; set; }
+        public double SizeOnDisk { get; set; }
+    }
+
+    public class TouchUpFilter : IPicture
+    {
+        public TouchUpFilter(IPicture picture)
+        {
+            Filters = picture.Filters.Union(new[] { "Touch-Up" }).ToList();
+            SizeOnDisk = picture.SizeOnDisk + picture.SizeOnDisk * .8;
+        }
+
+        public List<string> Filters { get; set; }
+        public double SizeOnDisk { get; set; }
+    }
+
+}
--- src/Decorator/Program.cs
+++ src/Decorator/Program.cs
@@ -1,9 +1,46 @@
-﻿namespace Decorator
+﻿using System;
+using Decorator.Instagramock;
+
+namespace Decorator
 {
     internal class Program
     {
+        private static IPicture _samplePicture;
+
         private static void Main()
         {
+            _samplePicture = new RawPicture(500D);
+            Report();
+
+            _samplePicture = new GrayScaleFilter(_samplePicture);
+            Report();
+
+            _samplePicture = new BlueEmphasizeFilter(_samplePicture);
+            Report();
+
+            _samplePicture = new RedEmphasizeFilter(_samplePicture);
+            Report();
+
+            _samplePicture = new TouchUpFilter(_samplePicture);
+            Report();
+
+            //_samplePicture = new TouchUpFilter(
+            //    new RedEmphasizeFilter(
+            //        new BlueEmphasizeFilter(
+            //            new GrayScaleFilter(_samplePicture)
+            //        )
+            //    )
+            //);
+            //Report();
+        }
+
+        private static void Report()
+        {
+            var report = $@"
+Filters: {string.Join(", ", _samplePicture.Filters)}
+Size: {_samplePicture.SizeOnDisk} KB
+===================================";
+            Console.WriteLine(report);
         }
     }
 }
