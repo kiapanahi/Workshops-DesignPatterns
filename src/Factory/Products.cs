9ab43e4b64622d0ad1d3dc7575e22ddcb129634b
Kia Panahi Rad <kia.panahirad@gmail.com>
2019-03-09 23:34:52 +0330

    Add sample product types

---
+++ src/Factory/Products.cs
@@ -0,0 +1,35 @@
+﻿namespace Factory
+{
+    internal abstract class Product
+    {
+    }
+
+    internal class InsuranceProduct : Product
+    {
+    }
+
+    internal class GhasedakProduct : Product
+    {
+    }
+
+    internal class ArvanCloudProduct : Product
+    {
+    }
+
+    internal class AbaciProduct : Product
+    {
+    }
+
+    internal class SejelProduct : Product
+    {
+    }
+
+    internal enum ProductCategory
+    {
+        Insurance,
+        Messenger,
+        IaaS,
+        CreditCalculation,
+        Security
+    }
+}
