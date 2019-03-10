2c3b4d3a46eaf726b935261e5c77f68bf7f38222
Kia Panahi Rad <kia.panahirad@gmail.com>
2019-03-10 10:33:53 +0330

    Add factory method usage sample

--- src/Factory/Program.cs
+++ src/Factory/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Linq;
+using Factory.Factory_Method;
 using Factory.Simple_Factory;
 
 namespace Factory
@@ -11,6 +12,7 @@ namespace Factory
             var productCategory = ShowMenu();
 
             #region Naive implementation
+
             Product product;
             switch (productCategory)
             {
@@ -31,23 +33,65 @@ namespace Factory
                     break;
                 default:
                     throw new ArgumentOutOfRangeException();
+
             }
+            Report("Naive Method", product);
+
             #endregion
 
             #region Using SimpleFactory
+
             var productViaSimpleFactory = new SimpleFactory().CreateProduct(productCategory);
+            Report("Using SimpleFactory", productViaSimpleFactory);
+
             #endregion
 
-            Console.WriteLine($"product: {product.GetType().Name}");
+            #region Using factory method
+
+            Product productViaFactoryMethod;
+            switch (productCategory)
+            {
+                case ProductCategory.Insurance:
+                    productViaFactoryMethod = new InsuranceStore().CreateProduct();
+                    break;
+                case ProductCategory.Messenger:
+                    productViaFactoryMethod = new MessengerStore().CreateProduct();
+                    break;
+                case ProductCategory.IaaS:
+                    productViaFactoryMethod = new IaaSStore().CreateProduct();
+                    break;
+                case ProductCategory.CreditCalculation:
+                    productViaFactoryMethod = new CreditCalculationStore().CreateProduct();
+                    break;
+                case ProductCategory.Security:
+                    productViaFactoryMethod = new SecurityStore().CreateProduct();
+                    break;
+                default:
+                    throw new ArgumentOutOfRangeException();
+            }
+            Report("Using FactoryMethod", productViaFactoryMethod);
+
+            #endregion
+
 
             CreateAndReportProduct(productCategory);
+        }
+
+        private static void Report(string issuer, Product product)
+        {
+            Console.WriteLine();
+            Console.WriteLine(new string('=', 50));
+            Console.WriteLine($"\t{issuer}");
+            Console.WriteLine($"\tproduct: {product.GetType().Name}");
+            Console.WriteLine(new string('=', 50));
+
         }
 
         private static void CreateAndReportProduct(ProductCategory productCategory)
         {
             Console.WriteLine();
             Console.WriteLine(new string('=', 50));
-            Console.WriteLine("\tCreating product somewhere else in application");
+            Console.WriteLine("\tCreating product somewhere in application");
 
             Product product;
             switch (productCategory)
