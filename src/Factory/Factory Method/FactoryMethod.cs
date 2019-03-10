184d1f061b567ec7c709a8f92ac3690de94e6268
Kia Panahi Rad <kia.panahirad@gmail.com>
2019-03-10 10:33:18 +0330

    Add factory method implementation

--- src/Factory/Factory Method/FactoryMethod.cs
+++ src/Factory/Factory Method/FactoryMethod.cs
@@ -1,7 +1,38 @@
-﻿namespace Factory.Factory_Method
+﻿using System;
+
+namespace Factory.Factory_Method
 {
-    public class FactoryMethod
+    internal abstract class ProductStore
     {
-        // TODO: Implement Factory method
+        public abstract Product CreateProduct();
+    }
+
+    internal class InsuranceStore : ProductStore
+    {
+        public override Product CreateProduct() => new InsuranceProduct();
+    }
+
+    internal class MessengerStore : ProductStore
+    {
+        public override Product CreateProduct() => new GhasedakProduct();
+
+    }
+
+    internal class IaaSStore : ProductStore
+    {
+        public override Product CreateProduct() => new ArvanCloudProduct();
+
+    }
+
+    internal class CreditCalculationStore : ProductStore
+    {
+        public override Product CreateProduct() => new AbaciProduct();
+
+    }
+
+    internal class SecurityStore : ProductStore
+    {
+        public override Product CreateProduct() => new SejelProduct();
+
     }
 }
