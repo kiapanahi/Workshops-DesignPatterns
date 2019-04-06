b5ec52298a41639f4e7540996771075f30f6746b
Kia Panahi Rad <kia.panahirad@gmail.com>
2019-04-06 23:38:11 +0430

    Code cleanup

--- src/Singleton/Program.cs
+++ src/Singleton/Program.cs
@@ -2,9 +2,9 @@
 
 namespace Singleton
 {
-    class Program
+    internal class Program
     {
-        static void Main(string[] args)
+        private static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
         }
