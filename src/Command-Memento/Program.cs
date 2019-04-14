51da1c1062eded6270947f21fdf9c7cefd361a90
Kia Panahi Rad <kia.panahirad@gmail.com>
2019-04-14 09:14:05 +0430

    Code cleanup

--- src/Command-Memento/Program.cs
+++ src/Command-Memento/Program.cs
@@ -2,11 +2,10 @@
 
 namespace CommandMemento
 {
-    class Program
+    internal class Program
     {
-        static void Main(string[] args)
+        private static void Main()
         {
-            Console.WriteLine("Hello World!");
         }
     }
 }
