3b4b1c8870db43c3828671701b4879068d822b2f
Kia Panahi Rad <kia.panahirad@gmail.com>
2019-05-06 08:58:50 +0430

    Add sample template method implementation

--- src/TemplateMethod/Program.cs
+++ src/TemplateMethod/Program.cs
@@ -2,11 +2,89 @@
 
 namespace TemplateMethod
 {
-    class Program
+    internal class Program
     {
-        static void Main(string[] args)
+        private static void Main()
         {
-            Console.WriteLine("Hello World!");
+            Brewery doranCafe = new CoffeeBrewery();
+
+            Brewery mamadTea = new HerbalBrewery();
+
+            Console.WriteLine(doranCafe.MakeBeverage());
+
+            Console.WriteLine(new string('-', 40));
+
+            Console.WriteLine(mamadTea.MakeBeverage());
+        }
+    }
+
+
+    internal abstract class Brewery
+    {
+        public double MakeBeverage()
+        {
+            BoilWater();
+            BrewStuff();
+            PourIntoContainer();
+            AddCondiments();
+            var price = Charge();
+
+
+            return price;
+        }
+
+        public abstract double Charge();
+
+        public abstract void AddCondiments();
+
+        private void PourIntoContainer()
+        {
+            Console.WriteLine("serving in a glass");
+        }
+
+        public abstract void BrewStuff();
+
+        private void BoilWater()
+        {
+            Console.WriteLine("boiling water");
+        }
+    }
+
+    internal class CoffeeBrewery : Brewery
+    {
+        public override double Charge()
+        {
+            Console.WriteLine("charging for coffee");
+            return 15d;
+        }
+
+        public override void AddCondiments()
+        {
+            Console.WriteLine("Adding milk, ruining the coffee");
+        }
+
+        public override void BrewStuff()
+        {
+            Console.WriteLine("making double espresso");
+        }
+    }
+
+    internal class HerbalBrewery : Brewery
+    {
+        public override double Charge()
+        {
+            Console.WriteLine("charging for funny stuff");
+            return 5d;
+        }
+
+        public override void AddCondiments()
+        {
+            Console.WriteLine("adding funny stuff");
+        }
+
+        public override void BrewStuff()
+        {
+            Console.WriteLine("دمنوش آرامش");
         }
     }
 }
