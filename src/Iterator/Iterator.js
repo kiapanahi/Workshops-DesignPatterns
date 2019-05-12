0f6bb2edf54527cc6d02bd9f2b16a320479f0514
Kia Panahi Rad <kia.panahirad@gmail.com>
2019-05-12 14:50:26 +0430

    Add JavaScript implenetation

---
+++ src/Iterator/Iterator.js
@@ -0,0 +1,21 @@
+﻿function range(start, end) {
+    return {
+        [Symbol.iterator]() {
+            console.log("iterator got called!");
+            return this;
+        },
+        next() {
+            console.log("next got called!");
+            if (start < end) {
+                return { value: start++, done: false };
+            }
+            return { value: end, done: true };
+        }
+    }
+}
+
+
+//usage:
+for (var item of range(1,10)) {
+    console.log(item);
+}
